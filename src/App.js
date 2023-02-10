e0a6ce5bfae9315703197f182c7579b6af5bc8c1
sanjeev9723 <sanjeev9723@gmail.com>
2023-02-10 09:34:54 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Error from './Pages/Error';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Login from './Pages/Login';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Error from "./Pages/Error";
+// import Navbar from "./components/Navbar";
+// import Header from "./components/Header";
+import Login from "./Pages/Login";
 
-import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
-import SharedLayout from './Pages/SharedLayout';
-import Dashboard from './Pages/Dashboard';
-import { useState } from 'react';
-
-
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import SharedLayout from "./Pages/SharedLayout";
+import Dashboard from "./Pages/Dashboard";
+import { useState } from "react";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
-     <BrowserRouter>
-     {/* <Header/> */}
-     <Routes>
+    <BrowserRouter>
+      {/* <Header/> */}
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login setUser={setUser}></Login>} />
+          <Route
+            path="dashboard"
+            element={<Dashboard user={user}></Dashboard>}
+          />
 
-     <Route path="/" element={<SharedLayout/>} >
-      <Route index element={<Home/>}/>
-     <Route path="about" element={<About/>} />
-     <Route path="login" element={<Login setUser={setUser}></Login>}/>
-     <Route path="dashboard"
-      element={<Dashboard user={user}></Dashboard>}/>
-
-     <Route path="*" element={<Error/>}/>
-     </Route>
-     </Routes>
-   
-     </BrowserRouter>
-
-  )
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 
 export default App;
